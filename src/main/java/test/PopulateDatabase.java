ea8dcbc534d4618580932228b9f897c4603f5630
jnorgini <jnorgini@gmail.com>
2024-06-25 16:12:42 -0300

    test: populating database

---
+++ src/main/java/test/PopulateDatabase.java
@@ -0,0 +1,53 @@
+package test;
+
+import java.util.ArrayList;
+
+import entities.Client;
+import services.ClientService;
+
+public class PopulateDatabase {
+
+	public static void main(String[] args) {
+		ClientService clienteService = new ClientService();
+
+		boolean truncated = clienteService.truncateClientsTable();
+		if (truncated) {
+			System.out.println("Table truncated successfully.\n");
+		} else {
+			System.out.println("Error truncating table.\n");
+		}
+
+		ArrayList<Client> existingClients = clienteService.getClients();
+		for (Client client : existingClients) {
+			boolean deletado = clienteService.deleteClient(client.getId());
+			if (deletado) {
+				System.out.println("Client with ID " + client.getId() + " deleted.");
+			} else {
+				System.out.println("Error deleting client with ID " + client.getId() + ".");
+			}
+		}
+
+		Client[] clients = { 
+				new Client(0, "John Smith", "john.smith@example.com"),
+				new Client(0, "Mary Johnson", "mary.johnson@example.com"),
+				new Client(0, "Charles Brown", "charles.brown@example.com")
+				};
+
+		for (Client client : clients) {
+			boolean created = clienteService.createClient(client);
+			if (created) {
+				System.out.println("Client " + client.getName() + " inserted successfully.");
+			} else {
+				System.out.println("Error inserting client " + client.getName() + ".");
+			}
+		}
+
+		ArrayList<Client> listedClients = clienteService.getClients();
+		System.out.println("\nRegistered clients:");
+		for (Client client : listedClients) {
+			System.out
+					.println("ID: " + client.getId() + ", Name: " + client.getName() + ", Email: " + client.getEmail());
+		}
+	}
+
+}
